dfd29e87cae5600d35bdce897ac26dc8420ae263
Fastering <73803835+Fastering18@users.noreply.github.com>
2021-02-07 02:41:39 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 import blackerz-wrapper as blackerz
 
-blackerz.authorize("UleMUVNvxLKnVydMblyOQB", "775363892167573535")
-goblox = blackerz.bot("717660707650273280")
+blackerz.authorize("auth v1", "bot developer id")
+your_bot = blackerz.bot("bot id")
 
-bot_data = goblox.bot_data()
+bot_data = your_bot.bot_data()
 print(bot_data)
 
-result = goblox.edit_data(short_description="generated_by_python_lib")
+result = your_bot.edit_data(short_description="generated_by_python_lib")
 print(result) # boolean indicates success or not
