ef08be1ecff91747258d1ecd5d17bfbcf6cc5519
Fastering <73803835+Fastering18@users.noreply.github.com>
2021-04-02 02:21:07 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,19 +4,23 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="blackerz-wrapper", 
-    version="0.0.15",
+    name="blackerz-wrapper", # Replace with your own username
+    version="0.0.2",
     author="Fastering18",
     author_email="brahmana081@gmail.com",
     description="Developer kit to interact with our API",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Fastering18/Blackerz-Wrapper-Python",
-    packages=setuptools.find_packages(),
+    project_urls={
+        "Bug Tracker": "https://github.com/Fastering18/Blackerz-SDK-Python/issues",
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3',
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires='>=2',
 )
