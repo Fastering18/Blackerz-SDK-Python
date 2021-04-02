9d82061012f6501cd0ffcba6681b743e8819ae45
Fastering <73803835+Fastering18@users.noreply.github.com>
2021-04-02 20:22:03 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,15 +1,15 @@
 import blackerz_wrapper as blackerz
 
-blackerz.authorize("V1 Auth")
-goblox = blackerz.bot("bot id or client")
+#blackerz.authorize("V1 Auth") # V1 Authorization, visit your profile on blackerz website to get your authorization v1
+#goblox = blackerz.bot("bot id or client")
 
-bot_data = goblox.bot_data()
-print(bot_data)
+#bot_data = goblox.bot_data() # get current bot data
+#print(bot_data)
 
-result = goblox.edit_data(short_description="Discord bot Goblox allow you to calculate number, moderate server, get account information from roblox, etc.")
-print(result) # boolean indicates success or not
+#result = goblox.edit_data(short_description="Discord bot Goblox allow you to calculate number, moderate server, get account information from roblox, etc.")
+#print(result) # boolean indicates success or not
 
-print(goblox.check_vote("775363892167573535")) # check if userid 775363892167573535 voted the bot
+#print(goblox.check_vote("775363892167573535")) # check if userid 775363892167573535 voted the bot
 
 
 print(blackerz.get_bot("717660707650273280")) # get bot data without auth
