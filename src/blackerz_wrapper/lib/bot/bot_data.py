9994227328c153605f25f7d80786b4c2ce2f4a31
Fastering <73803835+Fastering18@users.noreply.github.com>
2021-04-04 20:06:37 +0700

    Update bot_data.py

--- src/blackerz_wrapper/lib/bot/bot_data.py
+++ src/blackerz_wrapper/lib/bot/bot_data.py
@@ -3,7 +3,7 @@ from ..warns import BotNotFound
 from ..util.base_request import request, default_baseURL
 
 def get_bot_data(id, **anydata):
-    data = request(url=str(anydata.get("baseURL") or default_baseURL) + '/api/v1/bots/' + str(id)).get()
+    data = request(url="{0}/api/v1/bots/{1}".format(str(anydata.get("baseURL") or default_baseURL), str(id))).get()
     if data.status_code != 200:
         warnings.warn("Bot data not found", BotNotFound);
         return None
